46b5c079b13fcc7ccbf3797773bc1d4288f83b2c
Matt Long <matt.long@matthew-long.com>
2017-03-14 20:36:09 -0600

    Update to swift 3

--- PathMod/ViewController.swift
+++ PathMod/ViewController.swift
@@ -19,41 +19,49 @@ class ViewController: UIViewController {
         
         
         self.layer = CAShapeLayer()
-        self.layer.bounds = CGRectMake(0.0, 0.0, 200.0, 200.0)
-        self.layer.position = CGPointMake(300.0, 300.0)
+        self.layer.bounds = CGRect(x: 0.0, y: 0.0, width: 200.0, height: 200.0)
+        self.layer.position = CGPoint(x: 300.0, y: 300.0)
         
         // Generate a starting path that creates a square using
         // 200 points
-        var point = CGPointMake(0.0, 0.0)
+        var point = CGPoint(x: 0.0, y: 0.0)
         points.append(point)
         
         var i:Int = 0
-        for i = 0; i < 200; i = i + 10 {
-            point = CGPointMake(CGFloat(i), 0)
+        repeat {
+            point = CGPoint(x: CGFloat(i), y: 0)
             points.append(point)
-        }
+            i += 10
+        } while i < 200
 
-        for i = 0; i < 200; i = i + 10 {
-            point = CGPointMake(200.0, CGFloat(i))
+        i = 0
+        repeat {
+            point = CGPoint(x: 200.0, y: CGFloat(i))
             points.append(point)
-        }
+            i += 10
+        } while i < 200
         
-        for i = 200; i >= 0; i = i - 10 {
-            point = CGPointMake(CGFloat(i), 200.0)
+        i = 200
+        repeat {
+            point = CGPoint(x: CGFloat(i), y: 200.0)
             points.append(point)
-        }
+            i -= 10
+        } while (i > 0)
         
-        for i = 200; i >= 0; i = i - 10 {
-            point = CGPointMake(0, CGFloat(i))
+        
+        i = 200
+        repeat {
+            point = CGPoint(x: 0, y: CGFloat(i))
             points.append(point)
-        }
+            i -= 10
+        } while (i > 0)
         
         // Create a path with the array of points
         self.layer.path = points.path
         
         // Set the fill color and stroke color
-        self.layer.fillColor = UIColor.orangeColor().CGColor
-        self.layer.strokeColor = UIColor.blueColor().CGColor
+        self.layer.fillColor = UIColor.orange.cgColor
+        self.layer.strokeColor = UIColor.blue.cgColor
         
         self.layer.lineWidth = 4.0
         
@@ -62,7 +70,7 @@ class ViewController: UIViewController {
     }
 
     
-    @IBAction func didTapAnimate(sender:AnyObject) {
+    @IBAction func didTapAnimate(_ sender:AnyObject) {
         
         // Generate an array of points positioned around a circle that we 
         // conver to a path
@@ -73,14 +81,15 @@ class ViewController: UIViewController {
         let y0:CGFloat = 400.0
         var i:Int = 0
         
-        for i = 0; i < self.points.count; ++i {
+        repeat {
             // Little bit of trig to calculate where each point goes
             // evenly spaced around our destination circle.
             let x:CGFloat = x0 + r * cos(CGFloat(2) * CGFloat(M_PI) * CGFloat(i) / CGFloat(self.points.count))
             let y:CGFloat = y0 + r * sin(CGFloat(2) * CGFloat(M_PI) * CGFloat(i) / CGFloat(self.points.count))
             
-            newPoints.append(CGPointMake(x, y))
-        }
+            newPoints.append(CGPoint(x: x, y: y))
+            i += 1
+        } while i < self.points.count
 
         let animation = CABasicAnimation(keyPath: "path")
         animation.toValue = newPoints.path // Convert our new points array to a CGPathRef
@@ -90,7 +99,7 @@ class ViewController: UIViewController {
         animation.duration = 1.0
         animation.autoreverses = true
         
-        self.layer.addAnimation(animation, forKey: "path")
+        self.layer.add(animation, forKey: "path")
         
     }
 
@@ -113,17 +122,18 @@ extension Array where Element : Pointable {
         let bezier = UIBezierPath()
         
         if self.count > 0 {
-            bezier.moveToPoint(self[0].point)
+            bezier.move(to: self[0].point)
         }
         
-        for var i:Int = 1; i < self.count; ++i {
-            bezier.addLineToPoint(self[i].point)
-        }
+        var i:Int = 0
+        repeat {
+            bezier.addLine(to: self[i].point)
+            i += 1
+        } while i < self.count
         
-        bezier.closePath()
+        bezier.close()
         
-        return bezier.CGPath
+        return bezier.cgPath
         
     }
 }
-
